771baa3a2493ee383c405f5bf6114afa51cfa732
itsMohi <mohi222@pm.me>
2022-07-16 17:38:12 +0430

    first exercise

--- apps/micro/src/app/home/home.component.ts
+++ apps/micro/src/app/home/home.component.ts
@@ -8,27 +8,43 @@ import { v4 as uuidv4 } from 'uuid';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  price;
-  mode;
-  widgets: Widget[];
-
-  updateWidgetsAndRecalculateCost(widget: Widget) {
-    switch (this.mode) {
-      case 'create':
-        const newWidget = Object.assign({}, widget, { id: uuidv4() });
-        this.widgets = [...this.widgets, newWidget];
-        break;
-      case 'update':
-        this.widgets = this.widgets.map(_widget => widget.id === _widget.id
-          ? Object.assign({}, widget) : _widget);
-        break;
-      case 'delete':
-        this.widgets = this.widgets.filter(_widget => widget.id !== _widget.id);
-        break;
-      default:
-        break;
-    }
-    return this.widgets.reduce((acc, curr) => acc + curr.price, 0);
+  addWidget(widgets: Widget[], widget: Widget) {
+    const newWidget = Object.assign({}, widget, { id: uuidv4() });
+    widgets = [...widgets, newWidget];
+    return widgets;
   }
 
+  updateWidget(widgets: Widget[], widget: Widget) {
+    widgets = widgets.map((_widget) =>
+      widget.id === _widget.id ? Object.assign({}, widget) : _widget
+    );
+    return widgets;
+  }
+
+  deleteWidget(widgets: Widget[], widget) {
+    widgets = widgets.filter((_widget) => widget.id !== _widget.id);
+    return widgets;
+  }
+
+  updateWidgets(mode, widgets: Widget[], widget: Widget) {
+    switch (mode) {
+      case 'create':
+        return this.addWidget(widgets, widget);
+      case 'update':
+        return this.updateWidget(widgets, widget);
+      case 'delete':
+        return this.deleteWidget(widgets, widget);
+      default:
+        return widgets;
+    }
+  }
+
+  getTotalPrice(widgets: Widget[]) {
+    return widgets.reduce((acc, curr) => acc + curr.price, 0);
+  }
+
+  reCalculateTotal(mode, widgets: Widget[], widget: Widget) {
+    widgets = this.updateWidgets(mode, widgets, widget);
+    return this.getTotalPrice(widgets);
+  }
 }
